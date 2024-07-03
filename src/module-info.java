18b8275f8abd1dd883ace531321533a10e4736b3
Goob-bi <axn00bx@aol.com>
2024-07-03 10:29:56 -0700

    java/javafx 22

---
+++ src/module-info.java
@@ -0,0 +1,7 @@
+module cse360prototype.j22 {
+    requires transitive javafx.base;
+    requires transitive javafx.controls;
+    requires transitive javafx.graphics;
+
+    opens prototype to javafx.graphics;
+}
